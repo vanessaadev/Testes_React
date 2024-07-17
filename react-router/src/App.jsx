5ba47c233567f22c024603f4ad5640267382032d
unknown <vanessaaraujo77077@gmail.com>
2024-07-17 16:49:43 -0300

    utilização de props

--- react-router/src/App.jsx
+++ react-router/src/App.jsx
@@ -1,19 +1,43 @@
-import { BrowserRouter as Router } from 'react-router-dom'
-import { AppRoutes } from './Routes/AppRoutes'
-import { Navigation } from './Routes/Navigation'
-import './App.css'
+import React from 'react'
+
+function Saudacao(props) {
+  return <h1>Olá, {props.nome}!</h1>
+}
 
 function App() {
-
-  return (
-    <>
-      <Router>
-        {/* Barra de Navegação */}
-        <Navigation/>
-        <AppRoutes/>
-      </Router>
-    </>
+  return(
+    <div>
+      <Saudacao nome="João"/>
+      <Saudacao nome="Ana"/>
+    </div>
   )
 }
 
-export default App
+export default App;
+
+
+
+
+
+
+
+// ---------------Routes---------------------
+// import { BrowserRouter as Router } from 'react-router-dom'
+// import { AppRoutes } from './Routes/AppRoutes'
+// import { Navigation } from './Routes/Navigation'
+// import './App.css'
+
+// function App() {
+
+//   return (
+//     <>
+//       <Router>
+//         {/* Barra de Navegação */}
+//         <Navigation/>
+//         <AppRoutes/>
+//       </Router>
+//     </>
+//   )
+// }
+
+// export default App
